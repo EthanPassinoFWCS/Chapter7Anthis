067fdddde92ffb3fe26638aeb40aa49de2d7d6c4
EthanPassinoFWCS <ethan.passino@fwcsstudents.org>
2020-09-15 13:04:32 -0400

    This is the commit message, you can do either one for multiple files or different ones from each file. Then you hit the commit button. There may be more information you have to fill out. Fill it out and commit it.

--- TIY7-3.py
+++ TIY7-3.py
@@ -5,6 +5,7 @@ except:
     print("Invalid number.")
     exit(1)
 
-if(number % 10 == 0): print("Is a multiple of 10.")
-else: print("Is not a multiple of 10.")
-
+if number % 10 == 0:
+    print("Is a multiple of 10.")
+else:
+    print("Is not a multiple of 10.")
